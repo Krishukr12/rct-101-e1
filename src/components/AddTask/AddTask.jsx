87ebeb4aa6d6bb29318d0df0641afb4d3ef8c8f7
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2022-05-24 09:35:32 +0530

    remain design Work

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 import styles from "./addTask.module.css";
-const AddTask = ({addTask}) => {
+import styled from "styled-components";
+const Button = styled.button`
+  background-color: black;
+  color: white;
+`;
+const AddTask = ({ addTask }) => {
   // NOTE: do not delete `data-cy` key value pair
-  const [number,setNumber]=useState("");
+  const [number, setNumber] = useState("");
   return (
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" value={number} type="text"  placeholder="Add Task..." onChange={(e)=>{
-        setNumber(e.target.value);
-      }} />
-      <button data-cy="add-task-button" onClick={()=>{
-        addTask(number);
-        setNumber("")
-      }}>+</button>
+      <input
+        data-cy="add-task-input"
+        value={number}
+        type="text"
+        placeholder="Add Task..."
+        onChange={(e) => {
+          setNumber(e.target.value);
+        }}
+      />
+      <Button
+        data-cy="add-task-button"
+        onClick={() => {
+          addTask(number);
+          setNumber("");
+        }}
+      >
+        +
+      </Button>
     </div>
   );
 };
